036138987b7464ba0454ac1660686ceba0d1ccfe
Bastien Cordier <bastien.cordier78@gmail.com>
2023-11-29 21:19:19 +0100

    Fix bug import file v2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.scss";
 
 import Navbar from "./components/navbar.js";
-import Intro from "./containers/intro.js";
+import Intro from "./containers/home.js";
 import AboutMe from "./containers/aboutMe.js";
 import Projects from "./containers/projects.js";
 
